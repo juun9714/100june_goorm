90f19658c5d8dcd0af7ec4c56e7302ded14a3d02
June <lovekwon200@gmail.com>
2021-01-20 09:55:13 +0000

    1267

---
+++ cpp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int young(int n, int * arr){
+	int num=0;
+	for(int i =0;i<n;i++){
+		num+=(arr[i]/30)+1;
+	}
+	return 10*num;
+}
+
+int min(int n, int * arr){
+	int num=0;
+	for(int i =0;i<n;i++){
+		num+=(arr[i]/60)+1;
+	}
+	return 15*num;
+}
+int main(){
+	
+	int n;
+	cin>>n;
+	int * arr=new int[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	if(young(n,arr)<min(n,arr))
+		cout<<"Y "<<young(n,arr);
+	else if(young(n,arr)>min(n,arr))
+		cout<<"M "<<min(n,arr);
+	else
+		cout<<"Y M "<<young(n,arr);
+	return 0;
+}
